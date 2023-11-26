30092032112988df909dbca19f44ae417816d8e1
Kamaldeep Bhati <th.kamalsinghrajput@gmail.com>
2023-11-26 22:18:05 +0530

    Update autoFrida.sh

--- autoFrida.sh
+++ autoFrida.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 echo 
+echo -e '\e[31m
+    __    __  __  ____  _____    
+   /__\  (  )(  )(_  _)(  _  )   
+  /(__)\  )(__)(   )(   )(_)(    
+ (__)(__)(______) (__) (_____)   
+  ____  ____  ____  ____    __   
+ ( ___)(  _ \(_  _)(  _ \  /__\  
+  )__)  )   / _)(_  )(_) )/(__)\ 
+ (__)  (_)\_)(____)(____/(__)(__)
+
+\e]0m'
 												   
 echo -e "\e[33m By Kamaldeep Bhati (@DarkLotusKDB) <3\n\e[0m"
 
@@ -13,7 +24,7 @@ mkdir -p downloads
 
 arc="arm64"
 
-echo -e "\e[32m[+] Downloading latest Frida server\e[0m"
+echo -e "\n\n\e[32m[+] Downloading latest Frida server\e[0m"
             
             latestBuild=$(curl --silent "https://api.github.com/repos/frida/frida/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
             echo "Latest Frida Version: $latestBuild"
@@ -25,13 +36,13 @@ echo -e "\e[32m[+] Downloading latest Frida server\e[0m"
             mv frida-server-${latestBuild}-android-${arc} ../frida-server
             chmod +x ../frida-server
 
-echo -e "\e[32m[+] Configuring Frida server auto start\e[0m"
+echo -e "\n\n\e[32m[+] Configuring Frida server auto start\e[0m"
     cd
     mv .bashrc .oldbashrc &> /dev/null
     sed -i '/frida-server/d' .bashrc &> /dev/null
     sed -i '/H && sh/d' .bashrc &> /dev/null
     echo -e 'su -c "/data/data/com.termux/files/home/auto-launch-frida-server/frida-server -D &"\nsu -c "export PATH=/data/data/com.termux/files/usr/bin:$PATH && sh"' >> .bashrc
-
+echo -e '\nDone, All Set!!'
 
 echo
 echo -e "Thank You,\nKamaldeep Bhati (@DarkLotusKDB)"
